d20769b3bce5c3e3372d0624e2ef79c9bb540d93
Edison Asto <edison.asto@godata.com.ec>
2020-12-04 16:06:45 -0500

    cambio objecto programa.cs

--- RazorPagesTutorial/Program.cs
+++ RazorPagesTutorial/Program.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq;  
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +22,7 @@ namespace RazorPagesTutorial
                 {
                     webBuilder.UseStartup<Startup>();
                     // prueba 
+                    // prueba 2
                 });
     }
 }
